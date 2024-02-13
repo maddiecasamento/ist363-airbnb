4e69af9c62e0bc7ebe29e64f7ea5b273b8e4dc5b
maddiecasamento <119361341+maddiecasamento@users.noreply.github.com>
2024-02-13 12:18:51 -0500

    first commit

--- js/main.js
+++ js/main.js
@@ -29,27 +29,80 @@ closeBtn.addEventListener("click", function() {
 //     description: 'a beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.'
 // };
 
-//array
-const rooms = [
-    {
-        name: 'Luxury King Room',
-        price: 300,
-        guests: 2,
-        description: 'a beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.'
-    },
-    {
-        name: 'Luxury King Room',
-        price: 300,
-        guests: 2,
-        description: 'a beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.'
-    },
-    {
-        name: 'Luxury King Room',
-        price: 300,
-        guests: 2,
-        description: 'a beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.'
-       },
-    ]; // end of rooms
+// const room = {
+//     title: "Luxury King Room",
+//     price: 300,
+//     guests: 2,
+//     description: "a beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean."
+// };
+// console.log(room.title);
+
+// const roomsArr = [
+//     {
+//         title: "Luxury King Room"
+//         price: 300,
+//         guests: 2,
+//         description: "a beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.",
+//     },
+// ];
+
+// roomsArr.forEach(() => {
+//     console.log(room.title);
+// });
+
+//array of objects
+// const rooms = [
+//     {
+//         name: 'Luxury King Room',
+//         price: 300,
+//         guests: 2,
+//         description: 'a beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.'
+//     },
+//     {
+//         name: 'Luxury King Room',
+//         price: 300,
+//         guests: 2,
+//         description: 'a beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.'
+//     },
+//     {
+//         name: 'Luxury King Room',
+//         price: 300,
+//         guests: 2,
+//         description: 'a beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.'
+//        },
+//     ]; // end of rooms
+
+    function renderProperties(properties) {
+        properties.forEach((room) => {
+
+            const roomArticle = document.createElement('article');
+            roomArticle.classList.add('room');
+    
+            const roomNameElement = document.createElement('h3');
+            roomArticle.classList.add('room--name');
+            roomNameElement.textContent = room.name;
+    
+            const roomDescriptionElement = document.createElement('p');
+            roomDescriptionElement.classList.add('room--description');
+            roomDescriptionElement.textContent = room.description;
+    
+            const roomPriceElement = document.createElement('p');
+            roomPriceElement.textContent = `Price: ${room.price}`;
+    
+            const roomGuestsElement = document.createElement('p');
+            roomGuestsElement.textContext = `Guests: ${room.guests}`;
+    
+            roomArticle.appendChild(roomNameElement);
+            roomArticle.appendChild(roomDescriptionElement);
+            roomArticle.appendChild(roomPriceElement);
+            roomArticle.appendChild(roomGuestsElement);
+    
+            document.body.appendChild(roomArticle);
+    
+    
+        });
+
+    } // end of renderProperties
 
     rooms.forEach((room) => {
 
@@ -83,3 +136,9 @@ const rooms = [
 
 // create elements
 
+fetch('./js/properties.json')
+.then((response) => response.json())
+.then((data) => {
+    //console.log(data);
+    renderProperties(data);
+});
